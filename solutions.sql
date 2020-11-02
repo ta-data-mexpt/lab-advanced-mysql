a049903c5f676df087a4110b48d445a09a062578
RenataRmz <68484766+RenataRmz@users.noreply.github.com>
2020-11-01 20:00:25 -0600

    CUpdated

--- solutions.sql
+++ solutions.sql
@@ -1,7 +1,7 @@
 USE publications;
 #Challenge 1: Most Profiting Authors, Step 1
 CREATE TEMPORARY TABLE Royalty_advance
-select ti.title_id, au.au_id, ti.price * sa.qty * ti.royalty / 100 * ta.royaltyper / 100 sales_royalty, ti.advance* ta.royaltyper / 100 total_advance
+select ti.title_id, au.au_id, ti.advance, ta.royaltyper, ti.price * sa.qty * ti.royalty / 100 * ta.royaltyper / 100 sales_royalty
 from authors AU
 left join titleauthor TA
 on AU.au_id =  TA.au_id
@@ -12,19 +12,20 @@ on TI.title_id=sa.title_id;
 
 #Challenge 1: Most Profiting Authors, Step 2
 CREATE TABLE summary
-select title_id,au_id,sum(sales_royalty) royalty ,sum(total_advance) advance
+select title_id,au_id,advance, royaltyper,sum(sales_royalty) royalty
 from royalty_advance
 group by title_id,au_id;
 
+
 #Challenge 1: Most Profiting Authors, Step 3
-select au_id, royalty + advance PROFIT
+select au_id, royalty + advance*royaltyper/100 PROFIT
 from summary
 order by PROFIT DESC
 LIMIT 3;
 
 #Challenge 2: Alternative Solution
-select RO_AD.au_id, sum(RO_AD.sales_royalty) + sum(RO_AD.total_advance) PROFIT
-from (select ti.title_id, au.au_id, ti.price * sa.qty * ti.royalty / 100 * ta.royaltyper / 100 sales_royalty, ti.advance* ta.royaltyper / 100 total_advance
+select RO_AD.au_id, sum(RO_AD.sales_royalty) + RO_AD.advance* RO_AD.royaltyper / 100 PROFIT
+from (select ti.title_id, au.au_id, ti.advance, ta.royaltyper, ti.price * sa.qty * ti.royalty / 100 * ta.royaltyper / 100 sales_royalty
 from authors AU
 left join titleauthor TA
 on AU.au_id =  TA.au_id
@@ -38,8 +39,8 @@ LIMIT 3;
 
 #Challenge 3
 CREATE TABLE most_profiting_authors
-select RO_AD.au_id, sum(RO_AD.sales_royalty) + sum(RO_AD.total_advance) PROFIT
-from (select ti.title_id, au.au_id, ti.price * sa.qty * ti.royalty / 100 * ta.royaltyper / 100 sales_royalty, ti.advance* ta.royaltyper / 100 total_advance
+select RO_AD.au_id, sum(RO_AD.sales_royalty) + RO_AD.advance* RO_AD.royaltyper / 100 PROFIT
+from (select ti.title_id, au.au_id, ti.advance, ta.royaltyper, ti.price * sa.qty * ti.royalty / 100 * ta.royaltyper / 100 sales_royalty
 from authors AU
 left join titleauthor TA
 on AU.au_id =  TA.au_id
