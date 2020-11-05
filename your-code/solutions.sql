23204e0bdf0f5f37e3a5e390f4ef60701c76046b
StephanieCortes <step_0707@hotmail.com>
2020-11-04 18:28:55 -0600

    estos son los ejercicios terminados

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -30,5 +30,25 @@ GROUP BY sub_q_2.au_id
 ORDER BY "sales_royalty_per_author" ASC
 LIMIT 3;
 
+CREATE TEMPORARY TABLE publications.profits_per_author
+SELECT sub_q_2.au_id, SUM(sub_q_2.sales_royalty_per_author) 
+FROM(
+SELECT DISTINCT sub_q.au_id, sub_q.title_id, SUM(sub_q.sales_royalty) AS "sales_royalty_per_author"
+FROM (
+SELECT DISTINCT au.au_id, sa.title_id, ti.price * sa.qty * ti.royalty / 100 * ti_au.royaltyper / 100 AS "sales_royalty"
+FROM publications.authors AS au
+LEFT JOIN publications.titleauthor AS ti_au ON au.au_id=ti_au.au_id
+LEFT JOIN publications.titles AS ti ON ti_au.title_id=ti.title_id
+LEFT JOIN publications.sales AS sa ON ti.title_id = sa.title_id
+GROUP BY au.au_id,sa.title_id) as sub_q
+GROUP BY sub_q.au_id) AS sub_q_2
+GROUP BY sub_q_2.au_id
+ORDER BY "sales_royalty_per_author" DESC
+LIMIT 3;
+
+CREATE TABLE publications.most_profiting_authors
+SELECT *
+FROM publications.profits_per_author;
+
 
 
