e73b2ede4503e93442fbf973faa0f34e6d9f88ed
MenaGarcJ <menagarcj0@gmail.com>
2021-05-09 17:13:22 -0500

    Challenge 2 sin author id

--- solutions.sql
+++ solutions.sql
@@ -33,4 +33,30 @@ INNER JOIN titleauthor t
 ON a.au_id = t.au_id
 INNER JOIN titles ti
 ON t.title_id = ti.title_id
-GROUP BY title, au_id; /*
+GROUP BY title, au_id; */
+
+-- Challenge 2 [Alternative Solution] -- 
+
+SELECT Royalty_for_authors.Sales_Royalty + Advance_income_for_authors.INCOME_ADVANCE as Total_Income
+FROM Royalty_for_authors, Advance_income_for_authors
+ORDER BY Total_Income DESC;
+
+CREATE TEMPORARY TABLE Royalty_for_authors
+SELECT a.au_id, t.title_id, SUM((ti.price * s.qty * ti.royalty / 100) * (t.royaltyper / 100)) as Sales_Royalty, ti.title
+FROM authors a
+INNER JOIN titleauthor t
+ON a.au_id = t.au_id
+INNER JOIN titles ti
+ON t.title_id = ti.title_id
+INNER JOIN sales s
+ON ti.title_id = s.title_id
+GROUP BY title, au_id;
+
+CREATE TEMPORARY TABLE Advance_income_for_authors
+SELECT a.au_id, ti.title, ti.advance as INCOME_ADVANCE
+FROM authors a
+INNER JOIN titleauthor t
+ON a.au_id = t.au_id
+INNER JOIN titles ti
+ON t.title_id = ti.title_id
+GROUP BY title, au_id;
