870fc822a89b92d02cda2a44fa102605648c0690
MenaGarcJ <menagarcj0@gmail.com>
2021-05-09 16:41:19 -0500

    Profit authors

--- solutions.sql
+++ solutions.sql
@@ -1,6 +1,17 @@
 -- Challenge 1 -- 
 
-SELECT A.au_id as AUTHOR_ID, (Sales_Royalty + INCOME_ADVANCE) as Profits
+SELECT (Sales_Royalty + INCOME_ADVANCE) as Profits
+FROM 
+	(SELECT a.au_id as AUTHOR_ID, t.title_id, SUM((ti.price * s.qty * ti.royalty / 100) * (t.royaltyper / 100)) as Sales_Royalty, ti.title, ti.advance as INCOME_ADVANCE
+	FROM authors a
+	INNER JOIN titleauthor t
+	ON a.au_id = t.au_id
+	INNER JOIN titles ti
+	ON t.title_id = ti.title_id
+	INNER JOIN sales s
+	ON ti.title_id = s.title_id
+	GROUP BY title, a.au_id
+ ) income;
 
 -- Royalty for authors  -- 
 SELECT a.au_id, t.title_id, SUM((ti.price * s.qty * ti.royalty / 100) * (t.royaltyper / 100)) as Sales_Royalty, ti.title
