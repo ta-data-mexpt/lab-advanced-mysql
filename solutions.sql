8ccd6ef193bde2c1af660eedf2e16645bf70175e
MenaGarcJ <menagarcj0@gmail.com>
2021-05-09 17:18:13 -0500

    Challenge 3

--- solutions.sql
+++ solutions.sql
@@ -60,3 +60,42 @@ ON a.au_id = t.au_id
 INNER JOIN titles ti
 ON t.title_id = ti.title_id
 GROUP BY title, au_id;
+
+
+-- Challenge 3 -- 
+
+CREATE TABLE most_profiting_authors
+SELECT (Sales_Royalty + INCOME_ADVANCE) as PROFIT
+FROM 
+	(SELECT a.au_id as AUTHOR_ID, t.title_id, SUM((ti.price * s.qty * ti.royalty / 100) * (t.royaltyper / 100)) as Sales_Royalty, ti.title, ti.advance as INCOME_ADVANCE
+	FROM authors a
+	INNER JOIN titleauthor t
+	ON a.au_id = t.au_id
+	INNER JOIN titles ti
+	ON t.title_id = ti.title_id
+	INNER JOIN sales s
+	ON ti.title_id = s.title_id
+	GROUP BY title, a.au_id
+ ) income 
+ ORDER BY PROFIT DESC;
+
+-- Royalty for authors -- 
+/* SELECT a.au_id, t.title_id, SUM((ti.price * s.qty * ti.royalty / 100) * (t.royaltyper / 100)) as Sales_Royalty, ti.title
+FROM authors a
+INNER JOIN titleauthor t
+ON a.au_id = t.au_id
+INNER JOIN titles ti
+ON t.title_id = ti.title_id
+INNER JOIN sales s
+ON ti.title_id = s.title_id
+GROUP BY title, au_id;
+
+-- Advance income for authors --
+
+SELECT a.au_id, ti.title, ti.advance as INCOME_ADVANCE
+FROM authors a
+INNER JOIN titleauthor t
+ON a.au_id = t.au_id
+INNER JOIN titles ti
+ON t.title_id = ti.title_id
+GROUP BY title, au_id; /* 
