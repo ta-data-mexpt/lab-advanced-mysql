b44dddb69bdc60a3daac9feaffbd379d3ba71b92
MenaGarcJ <menagarcj0@gmail.com>
2021-05-09 13:36:30 -0500

    Avance lab - challenge 1

--- solutions.sql
+++ solutions.sql
@@ -1,8 +1,9 @@
-SELECT a.au_id, t.title_id, (ti.price * s.qty * ti.royalty / 100 * t.royaltyper / 100) as Royalty
+SELECT a.au_id, t.title_id, SUM(ti.price * s.qty * ti.royalty / 100 * t.royaltyper / 100) as Royalty
 FROM authors a
 INNER JOIN titleauthor t
 ON a.au_id = t.au_id
 INNER JOIN titles ti
 ON t.title_id = ti.title_id
 INNER JOIN sales s
-ON ti.title_id = s.title_id;
+ON ti.title_id = s.title_id
+GROUP BY au_id, title_id;
