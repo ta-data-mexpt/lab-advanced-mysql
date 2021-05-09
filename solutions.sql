7bc7636d57474b5f5512f26cd3cda360503dfaea
MenaGarcJ <menagarcj0@gmail.com>
2021-05-09 13:46:03 -0500

    Cambios

--- solutions.sql
+++ solutions.sql
@@ -1,4 +1,7 @@
-SELECT a.au_id, t.title_id, SUM(ti.price * s.qty * ti.royalty / 100 * t.royaltyper / 100) as Royalty
+-- Challenge 1 -- 
+
+-- CREATE TEMPORARY TABLE authors_royalty -- 
+SELECT a.au_id, t.title_id, SUM(ti.price * s.qty * ti.royalty / 100 * t.royaltyper / 100) as Sales_Royalty, ti.title
 FROM authors a
 INNER JOIN titleauthor t
 ON a.au_id = t.au_id
@@ -6,4 +9,4 @@ INNER JOIN titles ti
 ON t.title_id = ti.title_id
 INNER JOIN sales s
 ON ti.title_id = s.title_id
-GROUP BY au_id, title_id;
+GROUP BY title_id;
